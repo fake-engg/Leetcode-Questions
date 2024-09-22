02234ae0f07ec6660199665d324a1f3a1b131647
Shivam_175 <120707485+fake-engg@users.noreply.github.com>
2024-09-22 17:36:37 +0530

    updated optimized solution

--- LeetCode for SQL/184. Department Highest Salary.sql
+++ LeetCode for SQL/184. Department Highest Salary.sql
@@ -12,4 +12,15 @@ WHERE ranking =1
 
 -- Runtime: 1623ms
 
--- Looking for more optimized solution
+--Updated: More optimized solution
+
+SELECT Department, Employee, Salary
+FROM ( SELECT d.name as Department, e.name as Employee, e.salary,
+    DENSE_RANK() OVER(PARTITION BY d.name ORDER BY e.salary desc) ranking
+    FROM Employee e
+    JOIN Department d
+    ON e.departmentId=d.id
+	) x
+	WHERE ranking =1
+
+--Runtime: 636ms
